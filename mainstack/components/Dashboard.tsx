2fd829ed24d016025833a9eb41ec93af529db352
Nnamdi Okeke <onnamdialex@gmail.com>
2023-04-24 18:06:05 +0100

    :art: modified the sidebar

--- mainstack/components/Dashboard.tsx
+++ mainstack/components/Dashboard.tsx
@@ -1,6 +1,39 @@
 import React from "react";
 import Image from "next/image";
 
+const sidebar1 = [
+  {
+    title: "Dashboard",
+    logoUrl: "/images/dashboard.jpg",
+    subContents: [
+      { title: "item 1", logoUrl: "/images/edit.jpg" },
+      { title: "item 1", logoUrl: "/images/group.jpg" },
+      { title: "item 1", logoUrl: "/images/hourglass_empty.jpg" },
+    ],
+  },
+];
+
+const sidebar2 = [
+  {
+    title: "Others 1",
+
+    subContents: [
+      { title: "item 1", logoUrl: "/images/camera.jpg" },
+      { title: "item 1", logoUrl: "/images/delete.jpg" },
+    ],
+  },
+
+  {
+    title: "Others 2",
+
+    subContents: [
+      { title: "item 1", logoUrl: "/images/subscriptions.jpg" },
+      { title: "item 1", logoUrl: "/images/file.jpg" },
+      { title: "item 1", logoUrl: "/images/alarm.jpg" },
+    ],
+  },
+];
+
 const Dashboard = () => {
   return (
     <div>
@@ -16,7 +49,56 @@ const Dashboard = () => {
           />
         </div>
 
-        <div></div>
+        <div>
+          <ul className="sidebar-list1">
+            {sidebar1.map((item, index) => (
+              <li key={index}>
+                <Image
+                  priority
+                  src={item.logoUrl!}
+                  alt={`${item.title} Logo`}
+                  width={10}
+                  height={10}
+                />
+                <span>{item.title}</span>
+                <ul className="sidebar-list2">
+                  {item.subContents.map((subContent, index) => (
+                    <li key={index}>
+                      <Image
+                        src={subContent.logoUrl}
+                        alt={`${subContent.title} Logo`}
+                        width={10}
+                        height={10}
+                      />
+                      <span>{subContent.title}</span>
+                    </li>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </ul>
+
+          <ul className="sidebar-list1">
+            {sidebar2.map((item, index) => (
+              <li key={index}>
+                <span>{item.title}</span>
+                <ul className="sidebar-list2">
+                  {item.subContents.map((subContent, index) => (
+                    <li key={index}>
+                      <Image
+                        src={subContent.logoUrl}
+                        alt={`${subContent.title} Logo`}
+                        width={10}
+                        height={10}
+                      />
+                      <span>{subContent.title}</span>
+                    </li>
+                  ))}
+                </ul>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
