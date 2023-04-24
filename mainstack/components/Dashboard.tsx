c0360fa6cc9aeb40e739f0d7312e537e605e4a32
Nnamdi Okeke <onnamdialex@gmail.com>
2023-04-24 23:11:46 +0100

    :construction: body, work in progress

--- mainstack/components/Dashboard.tsx
+++ mainstack/components/Dashboard.tsx
@@ -34,54 +34,71 @@ const sidebar = [
 const Dashboard = () => {
   return (
     <div>
-      <div className="logo-sidebar">
-        <div>
-          <Image
-            priority
-            src="/images/mainstack-logo.jpg"
-            alt="mainstack-logo"
-            width={30}
-            height={30}
-            className="logo"
-          />
+      <div className="layout">
+        <div className="logo-sidebar">
+          <div>
+            <Image
+              priority
+              src="/images/mainstack-logo.jpg"
+              alt="mainstack-logo"
+              width={30}
+              height={30}
+              className="logo"
+            />
+          </div>
+
+          <div>
+            <ul className="sidebar-list1">
+              {sidebar.map((item, index) => (
+                <li key={index}>
+                  {/*render image if the logoUrl exists*/}
+                  {item.logoUrl && (
+                    <Image
+                      priority
+                      src={item.logoUrl!}
+                      alt={`${item.title} Logo`}
+                      width={10}
+                      height={10}
+                      className="image-title"
+                    />
+                  )}
+                  <span className={index === 0 ? "firstTitle" : ""}>
+                    {item.title}
+                  </span>
+
+                  <ul className="sidebar-list2">
+                    {item.subContents.map((subContent, index) => (
+                      <li className="subContent" key={index}>
+                        <Image
+                          src={subContent.logoUrl}
+                          alt={`${subContent.title} Logo`}
+                          width={10}
+                          height={10}
+                          className="image-title"
+                        />
+                        <span>{subContent.title}</span>
+                      </li>
+                    ))}
+                  </ul>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
 
-        <div>
-          <ul className="sidebar-list1">
-            {sidebar.map((item, index) => (
-              <li key={index}>
-                {/*render image if the logoUrl exists*/}
-                {item.logoUrl && (
-                  <Image
-                    priority
-                    src={item.logoUrl!}
-                    alt={`${item.title} Logo`}
-                    width={10}
-                    height={10}
-                    className="image-title"
-                  />
-                )}
-                <span className={index === 0 ? "firstTitle" : ""}>
-                  {item.title}
-                </span>
+        <div className="nav">
+          <h2>Dashboard</h2>
 
-                <ul className="sidebar-list2">
-                  {item.subContents.map((subContent, index) => (
-                    <li className="subContent" key={index}>
-                      <Image
-                        src={subContent.logoUrl}
-                        alt={`${subContent.title} Logo`}
-                        width={10}
-                        height={10}
-                        className="image-title"
-                      />
-                      <span>{subContent.title}</span>
-                    </li>
-                  ))}
-                </ul>
-              </li>
-            ))}
-          </ul>
+          <div className="dashboard-headers">
+            <div className="headers">
+              <h1>Good Morning, Blessing 🌤</h1>
+              <p className="headers-word">Check out your dashboard summary.</p>
+            </div>
+
+            <div className="headers-analytics">
+              <p className="word-analytics">view analytics</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
