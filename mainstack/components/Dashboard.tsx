6a2d68668d50809a3af9d7ed96de9d8ae7102149
Nnamdi Okeke <onnamdialex@gmail.com>
2023-04-24 21:41:53 +0100

    :lipstick: styles for sidebar update. improvements via refactoring of codes done

--- mainstack/components/Dashboard.tsx
+++ mainstack/components/Dashboard.tsx
@@ -8,25 +8,25 @@ const sidebar = [
     logoUrl: "/images/dashboard.jpg",
     subContents: [
       { title: "item 1", logoUrl: "/images/edit.jpg" },
-      { title: "item 1", logoUrl: "/images/group.jpg" },
-      { title: "item 1", logoUrl: "/images/hourglass_empty.jpg" },
+      { title: "item 2", logoUrl: "/images/group.jpg" },
+      { title: "item 3", logoUrl: "/images/hourglass_empty.jpg" },
     ],
   },
 
   {
     title: "Others 1",
     subContents: [
-      { title: "item 1", logoUrl: "/images/camera.jpg" },
-      { title: "item 1", logoUrl: "/images/delete.jpg" },
+      { title: "item 4", logoUrl: "/images/camera.jpg" },
+      { title: "item 5", logoUrl: "/images/delete.jpg" },
     ],
   },
 
   {
     title: "Others 2",
     subContents: [
-      { title: "item 1", logoUrl: "/images/subscriptions.jpg" },
-      { title: "item 1", logoUrl: "/images/file.jpg" },
-      { title: "item 1", logoUrl: "/images/alarm.jpg" },
+      { title: "item 6", logoUrl: "/images/subscriptions.jpg" },
+      { title: "item 7", logoUrl: "/images/file.jpg" },
+      { title: "item 8", logoUrl: "/images/alarm.jpg" },
     ],
   },
 ];
@@ -58,18 +58,22 @@ const Dashboard = () => {
                     alt={`${item.title} Logo`}
                     width={10}
                     height={10}
+                    className="image-title"
                   />
                 )}
-                <span>{item.title}</span>
+                <span className={index === 0 ? "firstTitle" : ""}>
+                  {item.title}
+                </span>
 
                 <ul className="sidebar-list2">
                   {item.subContents.map((subContent, index) => (
-                    <li key={index}>
+                    <li className="subContent" key={index}>
                       <Image
                         src={subContent.logoUrl}
                         alt={`${subContent.title} Logo`}
                         width={10}
                         height={10}
+                        className="image-title"
                       />
                       <span>{subContent.title}</span>
                     </li>
