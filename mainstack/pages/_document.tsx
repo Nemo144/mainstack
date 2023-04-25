08223db94d9d48193b33af1050e9066254b76a55
Nnamdi Okeke <onnamdialex@gmail.com>
2023-04-25 14:11:24 +0100

    :package: install a package for development

--- mainstack/pages/_document.tsx
+++ mainstack/pages/_document.tsx
@@ -1,13 +1,18 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* <script
+          async
+          src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"
+        ></script> */}
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
